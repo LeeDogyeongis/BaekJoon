37a54e1e3dcb44bf6f5344d4813f9474a2edbd2c
Lee Dogyeong <103121781+LeeDogyeongis@users.noreply.github.com>
2022-12-04 19:13:11 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int first, second;
+    scanf("%d", &first);
+    scanf("%d", &second);
+	printf("%d\n", first * (second % 10));
+	printf("%d\n", first * (second / 10 % 10));
+	printf("%d\n", first * (second / 100));
+	printf("%d\n", first * second);
+}
